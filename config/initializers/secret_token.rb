7ea94ee83508889483922a988d230586abbf68ce
Dan Kang <dan@dskang.com>
2012-05-17 03:12:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ptonfml::Application.config.secret_token = '9ac2956c2c46bac479f5f28ee419cf3616d62f57f666579b64eabfb83b612dd93a0b84a372c8825518c55cf4fa73ea3614173fb226603d706ef066bf5bf2189b'
