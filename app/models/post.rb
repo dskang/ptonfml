12fd16fd01d76be18df5180539a466d206aaa7c2
Dan Kang <dan@dskang.com>
2012-05-17 13:12:40 -0400

    Fix post slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :content
+  validates :content, presence: true
 
   def to_param
-    preview = content.split(' ')[0..6].join(' ').parameterize
+    words = content.split(' ')
+    words_in_preview = [words.length, 7].min
+    preview = words[0..(words_in_preview - 1)].join(' ').parameterize
     "#{id}-#{preview}"
   end
 end
