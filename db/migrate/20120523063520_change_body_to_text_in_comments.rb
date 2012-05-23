b9d8dffb03bc3da95d802a5a891adf49028c73a4
Dan Kang <dan@dskang.com>
2012-05-23 02:42:32 -0400

    Change comment body to text field instead of string

---
+++ db/migrate/20120523063520_change_body_to_text_in_comments.rb
@@ -0,0 +1,5 @@
+class ChangeBodyToTextInComments < ActiveRecord::Migration
+  def change
+    change_column :comments, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522212114) do
+ActiveRecord::Schema.define(:version => 20120523063520) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
-    t.string   "body"
+    t.text     "body",             :limit => 255
     t.integer  "commentable_id"
     t.string   "commentable_type"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.string   "ip"
   end
 
