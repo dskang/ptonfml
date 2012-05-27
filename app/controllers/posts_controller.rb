1227b9e517a7bd840f42f8d517182aadc6e6a218
Dan Kang <dan@dskang.com>
2012-05-27 02:56:20 -0700

    Paginate approval page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
   end
 
   def review
-    @posts = Post.recent
+    @posts = Post.recent.paginate(page: params[:page], per_page: 20)
 
     respond_to do |format|
       format.html
