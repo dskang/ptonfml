ac77f9e09b92c1bce5a1af62973d260887c6c706
Dan Kang <dan@dskang.com>
2012-06-06 15:10:49 -0700

    Updated annotation for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,22 @@
 #
 # Table name: posts
 #
-#  id         :integer         not null, primary key
-#  content    :text
-#  likes      :integer         default(0)
-#  dislikes   :integer         default(0)
-#  approved   :boolean         default(FALSE)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#  ip         :string(255)
-#  admin      :boolean         default(FALSE)
-#  type       :string(255)
+#  id                 :integer         not null, primary key
+#  content            :text
+#  likes              :integer         default(0)
+#  dislikes           :integer         default(0)
+#  approved           :boolean         default(FALSE)
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
+#  ip                 :string(255)
+#  admin              :boolean         default(FALSE)
+#  type               :string(255)
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  reviewed           :boolean         default(FALSE)
+#  phone              :string(255)
 #
 
 class Post < ActiveRecord::Base
